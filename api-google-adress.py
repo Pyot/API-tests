fcab74a0800629234d384b9ccb142859d5daea07
Pyot <piotr.a.ludwig@gmail.com>
2017-07-27 17:06:06 +0200

    Google maps API

--- api-google-adress.py
+++ api-google-adress.py
@@ -4,14 +4,28 @@ import json
 
 main_api ='http://maps.googleapis.com/maps/api/geocode/json?'
 
-address = 'czestochowa'
-url =  main_api + urllib.parse.urlencode({'address': address})
+address = 'czestochowa halo'
 
-json_data = requests.get(url).json()
-
-print(json_data)
-
-json_status =  json_data['long_name']
-
-
-print('API Status: ' + json_status)
+while True:
+    address = input('Address: ')
+    url =  main_api + urllib.parse.urlencode({'address': address})
+    
+    print(urllib.parse.urlencode({'address': address}))
+    
+    json_data = requests.get(url).json()
+    
+    #print(json_data)
+    
+    json_status =  json_data['status']
+    
+    
+    print('API Status: ' + json_status + '\n')
+    print(url)
+    
+    if json_status == 'OK': 
+        for each in json_data['results'][0]['address_components']:
+            print(each['long_name'])
+            
+        
+        formatted_address =  json_data['results'][0]['formatted_address']
+        print(formatted_address)
