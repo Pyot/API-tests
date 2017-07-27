028c6096420dddcb29261ef955a943ebb346d964
Pyot <piotr.a.ludwig@gmail.com>
2017-07-27 16:15:36 +0200

    Google mapis API

--- api-google-adress.py
+++ api-google-adress.py
@@ -1,2 +1,13 @@
-# -*- coding: utf-8 -*-
+import  urllib.parse
+import requests
+import json
 
+main_api ='http://maps.googleapis.com/maps/api/geocode/json?'
+
+address = 'czestochowa'
+url =  main_api + urllib.parse.urlencode({'address': address})
+
+json_data = requests.get(url).json()
+
+json_status =  json_data['status']
+print('API Status: ' + json_status)
